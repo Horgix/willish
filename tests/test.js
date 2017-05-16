95e4661e425bd6b5b32a060b30fd1a1658a8d851
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-05-16 16:34:28 +0200

    tests: add Basic valid POST on /wishes

--- tests/test.js
+++ tests/test.js
@@ -47,6 +47,57 @@ describe("GET on /wishes", function() {
   });
 });
 
+describe("Basic valid POST on /wishes", function() {
+  var apiResponse;
+
+  before(function (){
+    newWish = {
+      name: "New Keyboard",
+      link: "http://www.thekeyboardwaffleiron.com/"
+    };
+    apiResponse = chakram.post("http://127.0.0.1:5000/wishes", newWish);
+    return apiResponse;
+  });
+
+  it("should have status code 201", function () {
+    return expect(apiResponse).to.have.status(201);
+  });
+  it("should have 'application/json' as Content-Type", function () {
+    return expect(apiResponse).to.have.header("Content-Type",
+      "application/json")
+  });
+  it("should have a 'Server' header", function () {
+    return expect(apiResponse).to.have.header("Server");
+  });
+  it("should have a 'Date' header", function () {
+    return expect(apiResponse).to.have.header("Date");
+  });
+  it("should have a 'Content-Length' header", function () {
+    return expect(apiResponse).to.have.header("Content-Length");
+  });
+  it("should have valid JSON in body", function () {
+    return expect(apiResponse).to.have.schema({
+      "type": "object",
+      "required": ["acquired", "id", "link", "name"],
+      "properties": {
+        "acquired": { "type": "boolean" },
+        "id":       { "type": "integer" },
+        "link":     { "type": ["string"] },
+        "name":     { "type": ["string"] }
+      }
+    });
+  });
+  it("should have correct name", function () {
+    return expect(apiResponse).to.have.json("name", "New Keyboard");
+  });
+  it("should have correct link", function () {
+    return expect(apiResponse).to.have.json("link", "http://www.thekeyboardwaffleiron.com/");
+  });
+  it("should have a non-acquired status", function () {
+    return expect(apiResponse).to.have.json("acquired", false);
+  });
+});
+
 
   // TODO:
   // - incorrect request !
@@ -54,3 +105,4 @@ describe("GET on /wishes", function() {
   // shouldn't leak too much server info
   // HTTP version
   // Status Message (Created, OK, etc)
+  // Wrong Accept header
